c0f10acbb899e8c2b3e0ce30ccbefd4349e1cd6f
Alison Luis <alisonluisk@gmail.com>
2019-04-24 20:04:56 +0000

    Alterando sidebar

--- include/sidebar.php
+++ include/sidebar.php
@@ -5,7 +5,7 @@
   <div class="sidebar-brand-icon">
     <img src="../imagem/favicon-2.png"> 
   </div>
-  <div class="sidebar-brand-text mx-3">Chamados</div>
+  <div class="sidebar-brand-text mx-3">CHAMADOS</div>
 </a>
 
 <!-- Divider -->
@@ -15,26 +15,37 @@
 <li class="nav-item" id="liHome">
   <a class="nav-link" href="../pages/home">
     <i class="fas fa-fw fa-home"></i>
-    <span>Home</span></a>
+    <span>CHAMADOS</span></a>
 </li>
+
 <?php
-if($_SESSION['UsuarioNivel'] != 1 && $_SESSION['UsuarioNivel'] != 4) { 
-  echo '<li class="nav-item" id="liDashboard">
-    <a class="nav-link" href="../pages/dashBoard">
-    <i class="fas fa-tachometer-alt"></i>
-    <span>Dashboard</span></a>
-  </li>';
-}
+      if(($_SESSION['UsuarioNivel'] != 1 && $_SESSION['UsuarioNivel'] != 4) && !in_array($_SESSION['UsuarioID'], array('50','12','8','20','59'))) { 
+        echo '<li class="nav-item" id="liDash">
+                <a class="nav-link" href="../pages/dashBoard">
+                <i class="fas fa-tachometer-alt"></i>
+                <span>DASHBOARD</span></a>
+              </li>';
+      }elseif(in_array($_SESSION['UsuarioID'], array('50','12','8','20','59'))){
+        echo '<li class="nav-item" id="liDash">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseDash" aria-expanded="true" aria-controls="collapseTwo">
+          <i class="fas fa-tachometer-alt"></i>
+          <span>DASHBOARDS</span>
+        </a>
+        <div id="collapseDash" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="../pages/dashBoard">
+                <i class="fas fa-tachometer-alt"></i>
+                <span>DASHBOARD</span></a>
+                <hr class="sidebar-divider">
+        <a class="collapse-item" href="../pages/dashBoard2">
+              <i class="fas fa-chart-bar"></i>
+              <span>GERENCIAL</span></a>
+          </div>
+        </div>
+      </li>';
+      }
 ?>
-<?php
-if( in_array($_SESSION['UsuarioID'], array('50','12','8','20'))) { 
-  echo '<li class="nav-item" id="liDashboard">
-    <a class="nav-link" href="../pages/dashBoard2">
-    <i class="fas fa-tachometer-alt"></i>
-    <span>Gerencial</span></a>
-  </li>';
-}
-?>
+
 
 <!-- Divider -->
 <hr class="sidebar-divider">
@@ -48,21 +59,21 @@ if( in_array($_SESSION['UsuarioID'], array('50','12','8','20'))) {
   <li class="nav-item" id="liChamados">
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
       <i class="fas fa-fw fas fa-plus"></i>
-      <span>Chamado</span>
+      <span>CHAMADO</span>
     </a>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
         <h6 class="collapse-header">Adicionar novo:</h6>
-        <a class="collapse-item" href="#" id="adcChamado">Chamado</a>
-        <a class="collapse-item" href="chamadoespera">Chamado em espera</a>
+        <a class="collapse-item" href="#" id="adcChamado">CHAMADO</a>
+        <a class="collapse-item" href="chamadoespera">CHAMADO EM ESPERA</a>
         <?php 
           if($_SESSION['UsuarioNivel'] != 1) {
-            echo '<h6 class="collapse-header">Consulta:</h6>';
-            echo '<a class="collapse-item" href="chamados">Chamados</a>';
+            echo '<h6 class="collapse-header">CONSULTA:</h6>';
+            echo '<a class="collapse-item" href="chamados">CHAMADO</a>';
           }
           if($_SESSION['UsuarioNivel'] == 3) {
-            echo '<h6 class="collapse-header">Cadastrar:</h6>';
-            echo '<a class="collapse-item" href="categorias">Categorias</a>';
+            echo '<h6 class="collapse-header">CADASTRAR:</h6>';
+            echo '<a class="collapse-item" href="categorias">CATEGORIAS</a>';
           }
         ?>
       </div>
@@ -73,7 +84,7 @@ if( in_array($_SESSION['UsuarioID'], array('50','12','8','20'))) {
   <li class="nav-item" id="liRegistroErros">
     <a class="nav-link" href="../pages/dashException">
       <i class="fab fa-fw fa-stack-overflow"></i>
-      <span>Registro de erros</span></a>
+      <span>REGISTRO DE ERROS</span></a>
   </li>
 
 <!-- Nav Item - Relatórios Collapse Menu -->
@@ -82,17 +93,17 @@ if( in_array($_SESSION['UsuarioID'], array('50','12','8','20'))) {
       echo'<li class="nav-item" id="liRelatorio">
             <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
               <i class="far fa-fw fa-chart-bar"></i>
-              <span>Relatórios</span>
+              <span>RELATÓRIOS</span>
             </a>
             <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
               <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Usuário:</h6>
-                <a class="collapse-item" href="../pages/relatorio">Chamados por atendente</a>
-                <h6 class="collapse-header">Empresas:</h6>
-                <a class="collapse-item" href="dadosempresasapi">Dados emp. API</a>
-                <a class="collapse-item" href="empresasinertes">Dados emp. inertes API</a>
-                <a class="collapse-item" href="relatorioempre">Empresas solicitantes</a>
-                <a class="collapse-item" href="#" id="registroAtividadeEcf">Registro de atividades ECF</a>
+                <h6 class="collapse-header">USUÁRIO:</h6>
+                <a class="collapse-item" href="../pages/relatorio">CHAMADOS POR ATENDENTE</a>
+                <h6 class="collapse-header">EMPRESAS:</h6>
+                <a class="collapse-item" href="dadosempresasapi">DADOS EMP. API</a>
+                <a class="collapse-item" href="empresasinertes">DADOS EMP. INERTES API</a>
+                <a class="collapse-item" href="relatorioempre">EMPRESAS SOLICITANTES</a>
+                <a class="collapse-item" href="#" id="registroAtividadeEcf">REGISTRO DE ATIVIDADES ECF</a>
               </div>
             </div>
           </li>';
@@ -109,7 +120,7 @@ if( in_array($_SESSION['UsuarioID'], array('50','12','8','20'))) {
     echo'  <li class="nav-item" id="liPlantao">
               <a class="nav-link" href="../pages/plantao">
                 <i class="fas fa-fw fa-first-aid"></i>
-                <span>Plantão</span></a>
+                <span>PLANTÃO</span></a>
             </li>';
     }
   ?>
